3829269b2a0e1ffc613654470ac64741dd7288f8
Chris Watson <cawatson1993@gmail.com>
2020-01-28 15:01:37 -0700

    Add helper index to tests

--- test/default.test.ts
+++ test/default.test.ts
@@ -1,4 +1,9 @@
-import { hello } from '../src'
+// Copyright (c) 2020 Chris Watson
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import { hello } from '.'
 
 it('logs "Hello, world!" to the console', () => {
   const consoleSpy = jest.spyOn(console, 'log')
